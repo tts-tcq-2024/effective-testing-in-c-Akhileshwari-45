4c7c0c799a94c42de05e3ca379d76aa505cfc40b
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-11-04 11:01:12 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <assert.h>
 
-char size(int cms) {
+char size(int cms)
+{
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms < 38) 
+    {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms > 38 && cms < 42) 
+    {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if(cms > 42)
+    {
         sizeName = 'L';
     }
     return sizeName;
